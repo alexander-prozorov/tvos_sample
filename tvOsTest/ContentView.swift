1b60d2339a7e7056720991623c6938ea400f9abd
alexander-prozorov <alexander.prozorov@akvelon.com>
2020-09-23 14:51:48 +0300

    init

---
+++ tvOsTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tvOsTest
+//
+//  Created by Александр Прозоров on 23.09.2020.
+//  Copyright © 2020 Александр Прозоров. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
